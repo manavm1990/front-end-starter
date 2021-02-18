b29bbabc74def4443b5c10d04195dcc0c93e2c9d
Manav Misra <manavm@visionwebsoft.com>
2021-02-18 07:42:37 -0600

    :mute: warnings

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,9 +13,11 @@ module.exports = {
     "arrow-body-style": 1,
     "import/extensions": 0,
     "import/prefer-default-export": "warn",
+    "no-param-reassign": 1,
     "no-plusplus": ["error", { allowForLoopAfterthoughts: true }],
     "no-unused-expressions": ["error", { allowShortCircuit: true }],
     "no-unused-vars": "warn",
+    "object-shorthand": 1,
     "prefer-arrow-callback": "warn",
   },
 };
